0c648fc21737ec12c7a2e84963a1e57e7bde899e
Jorge Tordoya <jtordoya.bo@gmail.com>
2024-09-26 15:02:42 -0400

    module updater test1

--- libs/servercore/atxupdater.js
+++ libs/servercore/atxupdater.js
@@ -10,16 +10,12 @@ class AtxUpdater {
 
     servidor.get("/update", (req, res) => {
       console.log("update");
-      exec("cd /home/tracking-capture", (err, stdout1, stderr) => {
+      exec("cd /home/tracking-capture; git fetch; git pull;", (err, stdout1, stderr) => {
         console.log("stdout1:", stdout1);
-        exec("git fetch", (err, stdout2, stderr) => {
-            console.log("stdout2:", stdout2);
-            exec("git fetch", (err, stdout3, stderr) => {
+        
                 console.log("stdout3:", stdout3);
                 console.log("err:", err);
                 req.end("updated");
-            });            
-        });
       });
     });
     return servidor;
