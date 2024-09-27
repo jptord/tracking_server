e7a6b4c27f61260eb8d505ae1a346e950cfaf8c8
Jorge Tordoya <jtordoya.bo@gmail.com>
2024-09-27 10:59:50 -0400

    v0.0.2

--- libs/servercore/atxupdater.js
+++ libs/servercore/atxupdater.js
@@ -1,37 +1,36 @@
 const express = require("express");
 const { exec } = require("child_process");
 class AtxUpdater {
-  constructor() {
-    this.router = express.Router();
-  }
+    constructor() {
+        this.router = express.Router();
+    }
 
-  init() {
-    let servidor = this.router;
+    init() {
+        let servidor = this.router;
 
-    servidor.get("/update", (req, res) => {
-      console.log("update");
-      exec("cd /home/tracking-capture; git fetch; git pull;", (err, stdout1, stderr) => {
-        console.log("stdout1:", stdout1);
-        if (stdout1.includes("Updating")){
-            console.log("updated, and rebooting");
-            res.end("updated, and rebooting");
-            try{
-                console.log("trying rebooing");    
-                exec("reboot now", (err2, stdout2, stderr2) => {                
-                
-                //console.log("stdout2:",stdout2);
-            });
-            }catch(e){
-                console.log("try rebooing err:", e);    
+        servidor.get("/update", (req, res) => {
+        console.log("update");
+        exec("cd /home/tracking-capture; git fetch; git pull;", (err, stdout1, stderr) => {
+            console.log("stdout1:", stdout1);
+            if (stdout1.includes("Updating")){
+                console.log("updated, and rebooting");
+                res.end("updated, and rebooting");
+                try{
+                    console.log("trying rebooing");    
+                    exec("reboot now ", (err2, stdout2, stderr2) => {     
+                    //console.log("stdout2:",stdout2);
+                });
+                }catch(e){
+                    console.log("try rebooing err:", e);    
+                }
             }
-        }
-        console.log("err:", err);
-        console.log("nothing to update");
-        res.end("nothing to update");
-      });
-    });
-    return servidor;
-  }
+            console.log("err:", err);
+            console.log("nothing to update");
+            res.end("nothing to update");
+        });
+        });
+        return servidor;
+    }
 }
 
 module.exports = { AtxUpdater };
