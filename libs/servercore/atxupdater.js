80cd392e0e6e467de5b6fb6e37cbde3ec591a779
Jorge Tordoya <jtordoya.bo@gmail.com>
2024-09-27 15:39:43 -0400

    v0.1.4 restore data add

--- libs/servercore/atxupdater.js
+++ libs/servercore/atxupdater.js
@@ -111,8 +111,8 @@ class AtxUpdater {
         let cmds = [];
         
         //cmds.push(`sshpass -p ${scpData.pass} ssh ${scpData.user}@${scpData.host} ' mkdir ${scpData.base}/${app.name}_${isodate}_data'`);
-        folderData.forEach(f=>cmds.push(`sshpass -p scp -r ${scpData.user}@${scpData.host}:${scpData.base}/${app.name}_${isodate}_data/${f.replaceAll("./","")} ${scpData.pass} ${f}`));
-        filesData.forEach(f=>cmds.push(`sshpass -p scp ${scpData.user}@${scpData.host}:${scpData.base}/${app.name}_${isodate}_data ${scpData.pass} ${f}`));        
+        folderData.forEach(f=>cmds.push(`sshpass -p ${scpData.pass} scp -r ${scpData.user}@${scpData.host}:${scpData.base}/${app.name}_${isodate}_data/${f.replaceAll("./","")} ${scpData.pass} ${f}`));
+        filesData.forEach(f=>cmds.push(`sshpass -p ${scpData.pass} scp ${scpData.user}@${scpData.host}:${scpData.base}/${app.name}_${isodate}_data ${f}`));        
         this.executeSerialize(cmds,0,'',(response)=>{
             callback(response);
         });
