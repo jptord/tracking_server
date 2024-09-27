f786588b26de06183f6d444052b507adc973e34d
Jorge Tordoya <jtordoya.bo@gmail.com>
2024-09-27 11:13:11 -0400

    v0.0.4

--- libs/servercore/atxupdater.js
+++ libs/servercore/atxupdater.js
@@ -7,24 +7,30 @@ class AtxUpdater {
 
     init() {
         let servidor = this.router;
-
         servidor.get("/update", (req, res) => {
-        console.log("update");
-        exec("cd /home/tracking-capture; git fetch; git pull;", (err, stdout1, stderr) => {
-            console.log("stdout1:", stdout1);
-            if (stdout1.includes("Updating")){
-                console.log("updated, and rebooting app");
-                res.end("updated, and rebooting app");
-                try{
-                    process.exit();
-                }catch(e){
-                    console.log("try rebooing err:", e);    
+            console.log("update");
+            exec("cd /home/tracking-capture; git fetch; git pull;", (err, stdout1, stderr) => {
+                console.log("stdout:", stdout1);
+                if (stdout1.includes("Updating")){
+                    console.log("updated, and rebooting app");
+                    res.end("updated, and rebooting app");
+                    try{
+                        //process.exit();
+                        exec("supervisord stop restart",(err2,stdout2,stderr) => {
+                            console.log("supervisord stdout:", stdout2);
+                        });
+                            
+                    }catch(e){
+                        console.log("try rebooing err:", e);    
+                    }
                 }
-            }
-            console.log("err:", err);
-            console.log("nothing to update");
-            res.end("nothing to update");
+                console.log("err:", err);
+                console.log("nothing to update");
+                res.end("nothing to update");
+            });
         });
+        servidor.get("/restart", (req, res) => {
+            process.exit();
         });
         return servidor;
     }
